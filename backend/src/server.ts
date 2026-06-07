a0871011a37d8e0bac788b690d19a7601b86acae
rrit58 <rohitrajind58@gmail.com>
2026-06-07 16:56:41 +0530

    server fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,50 +1,7 @@
-import express from "express";
-import cors from "cors";
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-import emailTemplate from "./emailTemplate.ts";
-dotenv.config();
+import app from "./app.ts";
 
-const app = express();
+const port = process.env.PORT;
 
-app.use(cors());
-app.use(express.json());
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Server is running...");
-});
-
-app.post("/send-message", async (req, res) => {
-  const { fullName, email, message } = req.body;
-  try {
-    const info = await transporter.sendMail({
-      from: `"Portfolio" <${process.env.MAIL_USER}>`,
-      to: process.env.MAIL_USER,
-      subject: `New Message from Portfolio`,
-      html: emailTemplate(fullName, email, message),
-    });
-    res.status(200).json({ success: true, info });
-  } catch (err) {
-    console.error("Email Error:", err);
-    res.status(500).json({ success: false, err });
-  }
-});
-
-if (process.env.NODE_ENV === "development") {
-  const port = process.env.PORT;
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
-
-export default app;
